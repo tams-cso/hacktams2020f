f5d1a0bb6ea5f84711acfc7b6e99b417b98a5a1e
Jeongwoo Choi <iamchoij@gmail.com>
2020-07-26 22:02:04 -0500

    Made form work, lowered water line

--- src/components/mailing-list-form.js
+++ src/components/mailing-list-form.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './mailing-list-form.css';
 
 class MailingListForm extends Component {
-   constructor (props) {
+   constructor(props) {
       super(props);
 
       this.state = {
@@ -18,17 +18,17 @@ class MailingListForm extends Component {
 
    render = () => (
       <div id="mc_embed_signup" className='normal'>
-         <form action={/*TODO: get mc code*/""} className="validate" id="mc-embedded-subscribe-form" method="POST" name="mc-embedded-subscribe-form">
+         <form action={"https://hacktams.us17.list-manage.com/subscribe/post?u=6a68a324d1c36cdf2fd2dd8d7&amp;id=4981179801"} className="validate" id="mc-embedded-subscribe-form" method="POST" name="mc-embedded-subscribe-form">
             <div id="mc_embed_signup_scroll">
-               <input className="required email" id="mce-EMAIL" name="EMAIL" placeholder="sophie.the.duck@gmail.com" type="email" value={this.state.email} onChange={this.updateEmail} />
+               <input className="required email" id="mce-EMAIL" name="EMAIL" placeholder="Email address" type="email" value={this.state.email} onChange={this.updateEmail} />
                <input className="button" id="mc-embedded-subscribe" name="subscribe" type="submit" value="Get Updates" readOnly />
                <div className="clear" id="mce-responses">
-                  <div className="response" id="mce-error-response" style={{display: 'none'}}></div>
-                  <div className="response" id="mce-success-response" style={{display: 'none'}}></div>
+                  <div className="response" id="mce-error-response" style={{ display: 'none' }}></div>
+                  <div className="response" id="mce-success-response" style={{ display: 'none' }}></div>
                </div>
 
-               <div aria-hidden="true" style={{position: 'absolute', left: -5000}}>
-                  <input name={/*TODO: get mc code*/""} tabIndex="-1" type="text" value="" readOnly />
+               <div aria-hidden="true" style={{ position: 'absolute', left: -5000 }}>
+                  <input name={"b_6a68a324d1c36cdf2fd2dd8d7_4981179801"} tabIndex="-1" type="text" value="" readOnly />
                </div>
             </div>
          </form>
