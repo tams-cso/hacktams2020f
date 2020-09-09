e48a8b208efd4f154a605a59a8720cd8ec4e4767
zach <zachtang0123@gmail.com>
2020-09-09 14:31:18 -0500

    basic routing set up

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import React from 'react';
 import './fonts.css';
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 import Noise from './components/noise';
 import Landing from './components/landing';
 
 const App = () => (
-  <div>
-    <Noise />
-    <Landing />
-  </div>
+  <Router>
+    
+
+    <Switch>
+      
+      <Route path='/registration'>
+        <div>
+          Registration
+        </div>
+      </Route>
+      <Route path='/'>
+        <div>
+          <Noise />
+          <Landing />
+        </div>
+      </Route>
+    </Switch>
+  </Router>
 )
 
 export default App;
